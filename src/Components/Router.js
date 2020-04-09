d791cd33b8e9de5d6f5c4eae6b79618ef1f958e8
ricepotato <sukjun40@naver.com>
2020-04-09 23:23:58 +0900

    styled component header

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -4,10 +4,12 @@ import Home from "Routes/Home";
 import Detail from "Routes/Detail";
 import Tv from "Routes/Tv";
 import Search from "Routes/Search";
+import Header from "Components/Header";
 
 export default () => (
   <Router>
     <>
+      <Header></Header>
       <Route path="/" exact component={Home} />
       <Route path="/detail" exact component={Detail} />
       <Route path="/search" exact component={Search} />
