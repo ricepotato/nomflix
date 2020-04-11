f3a168bdbd448cb7758bb37bb96ff54a13f9b3bb
ricepotato <sukjun40@naver.com>
2020-04-11 22:34:16 +0900

    get movie id from props

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -8,6 +8,25 @@ export default class extends React.Component {
     loading: true,
   };
 
+  async componentDidMount() {
+    const {
+      match: {
+        params: { id },
+      },
+      history: { push },
+    } = this.props;
+    console.log(this.props);
+    const parseId = parseInt(id);
+    if (isNaN(parseId)) {
+      push("/");
+      return;
+    }
+    try {
+    } catch {
+    } finally {
+    }
+  }
+
   render() {
     const { result, error, loading } = this.state;
     return <DetailPresenter result={result} error={error} loading={loading} />;
