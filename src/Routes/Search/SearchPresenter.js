e31e176d588a9050c439a0edef06bb5ca57cd34e
ricepotato <sukjun40@naver.com>
2020-04-11 23:25:42 +0900

    presenter

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,1 +1,23 @@
-export default () => "Search";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const SaerchPresenter = ({
+  movieResults,
+  tvResults,
+  error,
+  loading,
+  searchTerm,
+  handleSubmit,
+}) => null;
+
+SaerchPresenter.propTypes = {
+  movieResults: PropTypes.array,
+  tvResults: PropTypes.array,
+  error: PropTypes.string,
+  loading: PropTypes.string,
+  searchTerm: PropTypes.string,
+  handleSubmit: PropTypes.func.isRequired,
+};
+
+export default SaerchPresenter;
--- src/Routes/Tv/TvPresenter.js
+++ src/Routes/Tv/TvPresenter.js
@@ -1,1 +1,16 @@
-export default () => "Tv";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const TvPresenter = ({ topRated, airingToday, popular, error, loading }) =>
+  null;
+
+TvPresenter.propTypes = {
+  topRated: PropTypes.array,
+  airingToday: PropTypes.array,
+  popular: PropTypes.array,
+  error: PropTypes.string,
+  loading: PropTypes.bool.isRequired,
+};
+
+export default TvPresenter;
