53de9e0736de3cfd6771ca85f23cd6813b3e3e5c
Sukjun Sagong <ricepotato40@gmail.com>
2020-04-14 22:14:23 +0900

    poster

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -18,21 +18,45 @@ const HomePresenter = ({ nowPlaying, upcoming, popular, error, loading }) =>
       {upcoming && upcoming.length > 0 && (
         <Section title="Upcoming Movies">
           {upcoming.map((movie) => (
-            <Poster></Poster>
+            <Poster
+              title={movie.original_title}
+              imageUrl={movie.poster_path}
+              id={movie.id}
+              key={movie.id}
+              isMovie={true}
+              rating={movie.vote_average}
+              year={movie.release_date && movie.release_date.substring(0, 4)}
+            ></Poster>
           ))}
         </Section>
       )}
       {nowPlaying && nowPlaying.length > 0 && (
         <Section title="Now Playing">
           {nowPlaying.map((movie) => (
-            <Poster></Poster>
+            <Poster
+              title={movie.original_title}
+              imageUrl={movie.poster_path}
+              id={movie.id}
+              key={movie.id}
+              isMovie={true}
+              rating={movie.vote_average}
+              year={movie.release_date && movie.release_date.substring(0, 4)}
+            ></Poster>
           ))}
         </Section>
       )}
       {popular && popular.length > 0 && (
         <Section title="Popular Movies">
           {popular.map((movie) => (
-            <Poster></Poster>
+            <Poster
+              title={movie.original_title}
+              imageUrl={movie.poster_path}
+              id={movie.id}
+              key={movie.id}
+              isMovie={true}
+              rating={movie.vote_average}
+              year={movie.release_date && movie.release_date.substring(0, 4)}
+            ></Poster>
           ))}
         </Section>
       )}
--- src/Routes/Tv/TvPresenter.js
+++ src/Routes/Tv/TvPresenter.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import Section from "Components/Section";
 import Loader from "Components/Loader";
 import Message from "Components/Message";
+import Poster from "Components/Poster";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -16,17 +17,47 @@ const TvPresenter = ({ topRated, airingToday, popular, error, loading }) =>
     <Container>
       {topRated && topRated.length > 0 && (
         <Section title="Top Rated Shows">
-          {topRated.map((show) => show.name)}
+          {topRated.map((show) => (
+            <Poster
+              title={show.original_name}
+              imageUrl={show.poster_path}
+              id={show.id}
+              key={show.id}
+              isMovie={false}
+              rating={show.vote_average}
+              year={show.first_air_date && show.first_air_date.substring(0, 4)}
+            ></Poster>
+          ))}
         </Section>
       )}
       {popular && popular.length > 0 && (
         <Section title="Popular Shows">
-          {popular.map((show) => show.name)}
+          {popular.map((show) => (
+            <Poster
+              title={show.original_name}
+              imageUrl={show.poster_path}
+              id={show.id}
+              key={show.id}
+              isMovie={false}
+              rating={show.vote_average}
+              year={show.first_air_date && show.first_air_date.substring(0, 4)}
+            ></Poster>
+          ))}
         </Section>
       )}
       {airingToday && airingToday.length > 0 && (
         <Section title="Airing Today Shows">
-          {airingToday.map((show) => show.name)}
+          {airingToday.map((show) => (
+            <Poster
+              title={show.original_name}
+              imageUrl={show.poster_path}
+              id={show.id}
+              key={show.id}
+              isMovie={false}
+              rating={show.vote_average}
+              year={show.first_air_date && show.first_air_date.substring(0, 4)}
+            ></Poster>
+          ))}
         </Section>
       )}
       {error && <Message text={error} color="#e74c3c" />}
