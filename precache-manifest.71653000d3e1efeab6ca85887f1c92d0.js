3cef7afe2d740f45437e1f31a781749386813b91
ricepotato <sukjun40@naver.com>
2022-02-24 01:44:59 +0900

    Updates

---
+++ precache-manifest.71653000d3e1efeab6ca85887f1c92d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02fa486c65a7f39728db9580ae2030e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a53317b6089e23b7a0a4",
+    "url": "/static/js/2.882171d1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.882171d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c86d17e8577108ea0383",
+    "url": "/static/js/main.e2079ea7.chunk.js"
+  },
+  {
+    "revision": "290827ea64af51895d4b",
+    "url": "/static/js/runtime-main.dee31e17.js"
+  },
+  {
+    "revision": "cd4cbcca10aae6d8fce546016e196d16",
+    "url": "/static/media/noPoster.cd4cbcca.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nomflix/precache-manifest.ba166ec90000b1838d43405736dcd522.js"
+  "/precache-manifest.71653000d3e1efeab6ca85887f1c92d0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nomflix/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
